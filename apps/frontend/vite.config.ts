aa8e91259fec079428aa1aa4fd7f99609df933e3
lubna-adb <h1101241062@student.untan.ac.id>
2026-04-08 20:03:38 +0700

    monorepo docker

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -3,24 +3,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite"
 import path from "path"
 
-// https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-  // 1. Muat env file berdasarkan 'mode' (development, production, dll.)
-  // npm run dev -> development, npm run build -> production
-  // process.cwd() adalah direktori akar proyek Anda
-  // .env.[mode].local (Prioritas tertinggi)
-  // .env.[mode]
-  // .env.local
-  // .env (Prioritas terendah)
   const env = loadEnv(mode, process.cwd(), '');
-
-  const check = env.VITE_CHECK;
-  if (!check) throw new Error("env is not detected");
-  console.log("Berhasil env:", check)
-
   return {
-    // Sekarang Anda bisa menggunakan variabel env di sini jika butuh, 
-    // misalnya untuk mengganti port secara dinamis:
     build: {
       sourcemap: true
     },
